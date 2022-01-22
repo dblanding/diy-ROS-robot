f5bdaed232cdc641bb512e7c39d839346d8a478d
dblanding <dblanding@gmail.com>
2022-01-22 04:53:14 -0500

    refactored wheels pkg to put Decoder class in its own module.

--- my_robot/wheels/src/encoder_publisher.py
+++ my_robot/wheels/src/encoder_publisher.py
@@ -9,7 +9,7 @@ Publish on topics:
     /left_ticks
     /act_vel
 
-Decoder class adapted from rotary_encoder example at:
+Decoder class from rotary_encoder.py example at:
 https://abyz.me.uk/rpi/pigpio/examples.html
 Hook up A and B so that values increase when moving forward.
 pigpio daemon must be running. Launch with 'sudo pigpiod'.
@@ -19,6 +19,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Int32, Float32
 import pigpio
+from rotary_encoder import Decoder
 
 MTR_DEBUG = False  # enable/disable printing of mtr pwm values
 
@@ -272,77 +273,6 @@ def right_enc_callback(tick):
     
     global right_pos
     right_pos += tick
-
-
-class Decoder:
-   """Class to decode mechanical rotary encoder pulses. """
-
-   def __init__(self, pi, gpioA, gpioB, callback):
-      """
-      Instantiate the class with the pi and gpios connected to
-      rotary encoder contacts A and B. The callback is
-      called when the rotary encoder is turned.  It takes
-      one parameter which is +1 for CW and -1 for CCW.
-      """
-
-      self.pi = pi
-      self.gpioA = gpioA
-      self.gpioB = gpioB
-      self.callback = callback
-
-      self.levA = 0
-      self.levB = 0
-
-      self.lastGpio = None
-
-      self.pi.set_mode(gpioA, pigpio.INPUT)
-      self.pi.set_mode(gpioB, pigpio.INPUT)
-
-      self.pi.set_pull_up_down(gpioA, pigpio.PUD_UP)
-      self.pi.set_pull_up_down(gpioB, pigpio.PUD_UP)
-
-      self.cbA = self.pi.callback(gpioA, pigpio.EITHER_EDGE, self._pulse)
-      self.cbB = self.pi.callback(gpioB, pigpio.EITHER_EDGE, self._pulse)
-
-   def _pulse(self, gpio, level, tick):
-      """
-      Decode the rotary encoder pulse.
-
-                   +---------+         +---------+      0
-                   |         |         |         |
-         A         |         |         |         |
-                   |         |         |         |
-         +---------+         +---------+         +----- 1
-
-              +---------+         +---------+           0
-              |         |         |         |
-         B    |         |         |         |
-              |         |         |         |
-         -----+         +---------+         +---------+ 1
-      """
-
-      if gpio == self.gpioA:
-         self.levA = level
-      else:
-         self.levB = level;
-
-      if gpio != self.lastGpio: # debounce
-         self.lastGpio = gpio
-
-         if   gpio == self.gpioA and level == 1:
-            if self.levB == 1:
-               self.callback(1)
-         elif gpio == self.gpioB and level == 1:
-            if self.levA == 1:
-               self.callback(-1)
-
-   def cancel(self):
-      """
-      Cancel the rotary encoder decoder.
-      """
-
-      self.cbA.cancel()
-      self.cbB.cancel()
 
 
 if __name__ == '__main__':
--- my_robot/wheels/src/motor_calibrator.py
+++ my_robot/wheels/src/motor_calibrator.py
@@ -6,7 +6,7 @@
 
 Saved data used to populate TRS_curve values for motors.
 
-Decoder class adapted from rotary_encoder example at:
+Decoder class from rotary_encoder.py example at:
 https://abyz.me.uk/rpi/pigpio/examples.html
 Hook up A and B so that values increase when moving forward.
 pigpio daemon must be running. Launch with 'sudo pigpiod'.
@@ -14,6 +14,7 @@ pigpio daemon must be running. Launch with 'sudo pigpiod'.
 
 import rospy
 import pigpio
+from rotary_encoder import Decoder
 
 # Save data in datafile
 datafile = '/home/ubuntu/catkin_ws/src/my_robot/wheels/data/tr_spd.csv'
@@ -78,78 +79,6 @@ def right_enc_callback(tick):
     
     global right_pos
     right_pos += tick
-
-
-class Decoder:
-   """Class to decode mechanical rotary encoder pulses. """
-
-   def __init__(self, pi, gpioA, gpioB, callback):
-      """
-      Instantiate the class with the pi and gpios connected to
-      rotary encoder contacts A and B. The callback is
-      called when the rotary encoder is turned.  It takes
-      one parameter which is +1 for CW and -1 for CCW.
-      """
-
-      self.pi = pi
-      self.gpioA = gpioA
-      self.gpioB = gpioB
-      self.callback = callback
-
-      self.levA = 0
-      self.levB = 0
-
-      self.lastGpio = None
-
-      self.pi.set_mode(gpioA, pigpio.INPUT)
-      self.pi.set_mode(gpioB, pigpio.INPUT)
-
-      self.pi.set_pull_up_down(gpioA, pigpio.PUD_UP)
-      self.pi.set_pull_up_down(gpioB, pigpio.PUD_UP)
-
-      self.cbA = self.pi.callback(gpioA, pigpio.EITHER_EDGE, self._pulse)
-      self.cbB = self.pi.callback(gpioB, pigpio.EITHER_EDGE, self._pulse)
-
-   def _pulse(self, gpio, level, tick):
-      """
-      Decode the rotary encoder pulse.
-
-                   +---------+         +---------+      0
-                   |         |         |         |
-         A         |         |         |         |
-                   |         |         |         |
-         +---------+         +---------+         +----- 1
-
-              +---------+         +---------+           0
-              |         |         |         |
-         B    |         |         |         |
-              |         |         |         |
-         -----+         +---------+         +---------+ 1
-      """
-
-      if gpio == self.gpioA:
-         self.levA = level
-      else:
-         self.levB = level;
-
-      if gpio != self.lastGpio: # debounce
-         self.lastGpio = gpio
-
-         if   gpio == self.gpioA and level == 1:
-            if self.levB == 1:
-               self.callback(1)
-         elif gpio == self.gpioB and level == 1:
-            if self.levA == 1:
-               self.callback(-1)
-
-   def cancel(self):
-      """
-      Cancel the rotary encoder decoder.
-      """
-
-      self.cbA.cancel()
-      self.cbB.cancel()
-
 
 if __name__ == '__main__':
 
